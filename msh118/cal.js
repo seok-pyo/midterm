dd2e23653b9c8d748fd8804ba436be6faa25c7e5
romingoon <romingoon@gmail.com>
2024-07-06 16:31:09 +0900

    [문승현] 2024/07/06 중간고사 제출

--- msh118/cal.js
+++ msh118/cal.js
@@ -5,7 +5,37 @@ export function cal(ym) {
 
   // 이 부분을 완성하시오.
 
+  const y = ym.slice(0, 4);
+  const m = ym.slice(5);
+
+  const firstDay = new Date(y, m - 1, 1);
+  const week = firstDay.getDay();
+
+  ret = Array.from({ length: week }, (_, i) => ' ');
+
+  while (firstDay.getMonth() === m - 1) {
+    ret.push(firstDay.getDate());
+    firstDay.setDate(firstDay.getDate() + 1);
+  }
+
   console.log(`${m}월 ${y}`.padStart(21 / 2 + 4, ' '));
-  console.log([...WEEKS].map(w => w.padStart(2, ' ')).join(''));
-  console.log(ret);
+  console.log([...WEEKS].map((w) => w.padStart(2, ' ')).join(''));
+  console.log(
+    ret
+      .map((d, i) => {
+        let delim = (i + 1) % 7 === 0 ? '\n' : ' ';
+        d = d === 0 ? '' : d < 10 ? ` ${d}` : d;
+        return `${d}${delim}`;
+      })
+      .join('')
+  );
 }
+
+console.log('\n\n======================\n');
+cal('2024-2');
+console.log('-'.repeat(21), '\n');
+cal('2024-7');
+console.log('-'.repeat(21), '\n');
+cal('2024-9');
+
+console.log('\n\n======================\n');
--- msh118/reduce.js
+++ msh118/reduce.js
@@ -1,3 +1,41 @@
+import assert from 'assert';
+
 export const reduce = (array, fn, initValue) => {
   // 이 부분을 작성하시오.
+  let i = 0;
+  let result = initValue ?? ((i += 1), array[0]);
+  for (; i < array.length; i += 1) {
+    result = fn(result, array[i]);
+  }
+  return result;
 };
+
+const Hong = { id: 1, name: 'Hong' };
+const Kim = { id: 2, name: 'Kim' };
+const Lee = { id: 3, name: 'Lee' };
+let users1 = [Hong, Kim, Lee];
+
+const hong = { id: 1, name: 'Hong', dept: 'HR' };
+const kim = { id: 2, name: 'Kim', dept: 'Server' };
+const lee = { id: 3, name: 'Lee', dept: 'Front' };
+const park = { id: 4, name: 'Park', dept: 'HR' };
+const ko = { id: 7, name: 'Ko', dept: 'Server' };
+const loon = { id: 6, name: 'Loon', dept: 'Sales' };
+const choi = { id: 5, name: 'Choi', dept: 'Front' };
+const users = [hong, kim, lee, park, ko, loon, choi];
+
+const a10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+assert.deepStrictEqual(
+  reduce(a10, (acc, cur) => acc + cur, 0),
+  a10.reduce((acc, cur) => acc + cur, 0)
+);
+
+assert.deepStrictEqual(
+  reduce(users1, (acc, user) => acc + user.name),
+  users1.reduce((acc, user) => acc + user.name)
+);
+
+assert.deepStrictEqual(
+  reduce([1, 2, 3, 4, 5], (acc, cur) => acc * cur),
+  120
+);
