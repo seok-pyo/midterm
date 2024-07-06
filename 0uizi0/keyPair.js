4cb2b90334b4bae734a27ae8caa62f1bcf7c8c2f
0uizi0 <0uizi0@naver.com>
2024-07-06 16:29:06 +0900

    김은정_중간평가

--- 0uizi0/keyPair.js
+++ 0uizi0/keyPair.js
@@ -1,3 +1,8 @@
 export const keyPair = (arr, n) => {
-  // 이 부분을 작성하시오.
+  const val_idx = {} // {val: idx}
+  for (let i = 0; i <= arr.length; i += 1) {
+    const v = arr[i]
+    if (val_idx[v] !== undefined) return [val_idx[v], i]
+    val_idx[n/v] ??= i
+  }
 };
