42a4061827c9e6ca2957d6573616286bd6a25bd2
김성원 <one@gimseong-won-ui-MacBookPro.local>
2024-07-06 16:38:06 +0900

    김성원

--- kswus8502/keyPair.js
+++ kswus8502/keyPair.js
@@ -1,3 +1,11 @@
 export const keyPair = (arr, n) => {
-  // 이 부분을 작성하시오.
+  const val_idx = {};
+
+  for (let i = 0; i < arr.length; i += 1){
+    const val = arr[i];
+
+    if(val_idx[val]) return [val_idx[val], i];
+
+    val_idx[n / val] = i;
+  }
 };
